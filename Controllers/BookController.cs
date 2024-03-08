3b7c5d23e83319366fcee6b7c59382e5297acff7
gencaypolat <gencaypolat@gmail.com>
2024-03-08 03:09:51 +0300

    FromQuery and FromRoute added

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ControllersExampleProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,18 +13,12 @@ namespace ControllersExampleProject.Controllers
     {
         [Route("bookstore/{bookid?}/{isloggedin?}")]
         // Url: /bookstore?bookid=10&isloggedin=true
-        public IActionResult Index(int? bookid, bool? isloggedin)
+        public IActionResult Index([FromQuery] int? bookid, [FromRoute] bool? isloggedin, Book book)
         {
             //Book id should be applied
             if (bookid.HasValue == false)
             {
                 return BadRequest("Book id is not supplied");
-            }
-
-            //Book id can't be empty
-            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
-            {
-                return BadRequest("Book id  can't be null or empty");
             }
 
             //Book id should be between 1 to 1000
---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace ControllersExampleProject.Models
+{
+    public class Book
+    {
+        //[FromQuery]
+        public int? BookId { get; set; }
+        public string? Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book object - Book id: {BookId}, Author: {Author}";
+        }
+    }
+}
+

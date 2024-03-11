855ff131d83af702ce94d65bb3a7de5a16f02906
gencaypolat <gencaypolat@gmail.com>
2024-03-11 18:48:42 +0300

    Model Validations added

---
+++ Controllers/PersonNewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControllersExampleProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ControllersExampleProject.Controllers
+{
+    public class PersonNewController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(PersonNew person)
+        {
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage).ToList());
+
+                return BadRequest(errors);
+            }
+
+            return Content($"{person}");
+        }
+    }
+}
+
---
+++ Models/PersonNew.cs
@@ -0,0 +1,38 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+namespace ControllersExampleProject.Models
+{
+    public class PersonNew
+    {
+        [Required(ErrorMessage = "{0} can't be empty or null")]
+        [Display(Name = "Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
+        [RegularExpression("^[A-Za-z .]+$", ErrorMessage = "{0} should contain alphabets, space and dot (.)")]
+        public string? PersonName { get; set; }
+
+        [EmailAddress(ErrorMessage = "{0} should be proper Email")]
+        [Required(ErrorMessage = "{0} should be a proper email address")]
+        public string? Email { get; set; }
+
+        [Phone(ErrorMessage = "{0} should be 10 digits")]
+        //[ValidateNever]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Compare("Password", ErrorMessage = "{0} and {1} do not match")]
+        public string? ConfirmPassword { get; set; }
+
+        [Range(0, 999.99, ErrorMessage = "{0} should be between ${1} and ${2}")]
+        public double? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object - Person name: {PersonName}, Email: {Email}, Phone: {Phone}, Password: {Password}, Confirm Password: {ConfirmPassword}, Price: {Price}";
+        }
+    }
+}
